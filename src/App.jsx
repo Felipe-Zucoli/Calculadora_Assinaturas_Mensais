f5a3592dcfe0e9397acb37ae1e7a0e66c76b0cd1
Felipe-Zucoli <zucolifelipe@gmail.com>
2024-10-07 14:49:03 -0300

    Adicionado logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 import ListApps from './Components/ListApps'
+import Nav from './Components/Nav'
 
 function App() {
   return (
     <div className='Body'>
+      <Nav/>
       <h1 className='Titulo'>Calculadora de Assinaturas Mensais</h1>
       <p className='TextoAss'>Selecione primeiro sua assinatura.</p>
       <ListApps/>
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,12 @@
+import Fz from "../Images/Fz.png"
+import "./Nav.css"
+
+const Nav = () => {
+    return (
+        <nav>
+    <a href="https://github.com/Felipe-Zucoli" target="_blank"><img src={Fz} alt="Logo" className="ImgLogo"/></a>
+    </nav>
+)
+}
+
+export default Nav;
